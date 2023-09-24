f94fb7d6fc25c35a042a88cf1475a291ae9d41cd
Kejczorek <beka89t@gmail.com>
2023-09-25 01:24:28 +0200

    deploy vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: fileURLToPath(new URL("./", import.meta.url)) }],
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: "@", replacement: fileURLToPath(new URL("./", import.meta.url)) }],
+    },
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/google-saper-clone/";
+  }
+
+  return config;
 });
